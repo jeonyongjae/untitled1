7f8ef4dc8b196d1d2417b43b0fabd8dd2781183b
YongJae Jeon <dydzmxkf@naver.com>
2024-10-22 19:32:38 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ import requests
 url = 'https://github.com/jeonyongjae/LG-Academy.git/Quiz 1.ipynb'
 save_path = '/content/drive/MyDrive/Quiz 1.ipynb'
 
-# response = requests.get(url)
-# with open(save_path, 'wb') as f:
-#     f.write(response.content)
+response = requests.get(url)
+with open(save_path, 'wb') as f:
+    f.write(response.content)
 
-# print(f"저장!!: {save_path}")
+print(f"저장!!: {save_path}")
 
 # import zipfile
 
