199777967d8466d01e4746d95553eb0fc5702c5a
YongJae Jeon <dydzmxkf@naver.com>
2024-10-22 19:02:59 +0900

    Quiz 불러오기.py

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print('hello world')
+from google.colab import drive
+drive.mount('/content/drive')
+
+import requests
+
+url = 'https://github.com/policyoptimizer/241022_malfunction_prediction/raw/main/24%EB%85%84_AI_Code(%EC%8B%A4%EC%8A%B5%EC%BD%94%EB%93%9C)_2%EC%B0%A8%EC%88%98.zip'
+save_path = '/content/drive/MyDrive/24년_AI_Code_2차수.zip'
+
+response = requests.get(url)
+with open(save_path, 'wb') as f:
+    f.write(response.content)
+
+print(f"저장!!: {save_path}")
+
+import zipfile
+
+unzip_folder_path = '/content/drive/MyDrive/24년_AI_Code_2차수/'
+with zipfile.ZipFile(save_path, 'r') as zip_ref:
+    zip_ref.extractall(unzip_folder_path)
+
+print(f"압축 해제: {unzip_folder_path}")
