6c4209cdfd9799ed468ab655fdcf92f31cde1c95
YongJae Jeon <dydzmxkf@naver.com>
2024-10-22 19:30:35 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,19 +4,19 @@ drive.mount('/content/drive')
 
 import requests
 
-url = 'https://github.com/jeonyongjae/LG-Academy.git/24%EB%85%84_AI_Code(%EC%8B%A4%EC%8A%B5%EC%BD%94%EB%93%9C)_2%EC%B0%A8%EC%88%98.zip'
-save_path = '/content/drive/MyDrive/AI_Code_Quiz1.zip'
+url = 'https://github.com/jeonyongjae/LG-Academy.git/Quiz 1.ipynb'
+save_path = '/content/drive/MyDrive/Quiz 1.ipynb'
 
-response = requests.get(url)
-with open(save_path, 'wb') as f:
-    f.write(response.content)
+# response = requests.get(url)
+# with open(save_path, 'wb') as f:
+#     f.write(response.content)
 
-print(f"저장!!: {save_path}")
+# print(f"저장!!: {save_path}")
 
-import zipfile
+# import zipfile
 
-unzip_folder_path = '/content/drive/MyDrive/AI_Code'
-with zipfile.ZipFile(save_path, 'r') as zip_ref:
-    zip_ref.extractall(unzip_folder_path)
+# unzip_folder_path = '/content/drive/MyDrive/AI_Code'
+# with zipfile.ZipFile(save_path, 'r') as zip_ref:
+#     zip_ref.extractall(unzip_folder_path)
 
-print(f"압축 해제: {unzip_folder_path}")
+# print(f"압축 해제: {unzip_folder_path}")
